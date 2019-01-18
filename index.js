6164f62a75fcb46c101e6743b0bc0ec1851aba3c
Chris Nunez <cenunez47@gmail.com>
2019-01-18 18:56:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ function getRepositories() {
 
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText);
-  const repoList = `<ul>${repos.map(rep => '<li><a href="' +rep.html_url + '">' + rep.name + '</li>').join('')}</a></il>`;
+  const repoList = '<ul>' + repos.map(rep => {
+    return (`
+      <li>
+        <h2><a href="${rep.html_url}">${rep.name}</a></h2>
+        <p>Watchers: ${rep.watchers_count}</p>
+        <p>Forks: ${rep.forks_count}</p>
+        <p>Issues: ${rep.open_issues_count}</p>
+      </il>`
+    )
+  }).join('') + '</ul>'
   document.getElementById('repositories').innerHTML = repoList;
 }
